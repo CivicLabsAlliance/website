b9997a3c131b3ac404eb00a8e6eb86716f79082d
Bentley Hensel <bentleyhensel@gmail.com>
2024-07-29 15:25:19 -0400

    Basic submission form
    Signed-off-by: Bentley Hensel <bentleyhensel@gmail.com>

---
+++ js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const organization = document.getElementById('organization').value;
+        const message = document.getElementById('message').value;
+
+        const data = {
+            name: name,
+            email: email,
+            organization: organization,
+            message: message
+        };
+
+        const url = 'https://script.google.com/macros/s/AKfycbwzbUFLDx1LFWCrUbVNBJ7Nvjm7QBqYUgOXIy0xGlgaZUX24nPVdarUVeKlyIu687fysg/exec';
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.text())
+        .then(data => {
+            alert('Form submitted successfully!');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('There was an error submitting the form.');
+        });
+    });
+});
